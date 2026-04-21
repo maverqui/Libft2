a6306c4a1c1cc172ee795078d184bff62bca2de5
Joseph Sommet <jsommet@student.42.fr>
2026-04-21 15:46:03 +0200

    ca avance

--- libft.h
+++ libft.h
@@ -1,10 +1,14 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
+#include <stdio.h>
 
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int 	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+int		ft_toupper(int c);
 
 #endif
